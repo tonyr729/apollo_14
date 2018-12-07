1c8bce183bab4ab749e00fd2be5822c1845c367c
Tony Robertson <tonyr729@gmail.com>
2018-12-07 11:03:52 -0700

    Add model test for average age method

--- spec/models/astronaut_spec.rb
+++ spec/models/astronaut_spec.rb
@@ -11,4 +11,14 @@ describe Astronaut, type: :model do
     it { should have_many :astronaut_missions}
     it { should have_many :missions}
   end
+
+  describe 'Class Methods' do
+    it 'should be able to average age' do
+      astro_1 = Astronaut.create!(name: "Buzz Aldron", age: 60, job: "Badass")
+      astro_2 = Astronaut.create!(name: "Tony Robertson", age: 32, job: "Dev")
+      astro_3 = Astronaut.create!(name: "Drake", age: 10, job: "Entertainment")
+
+      expect(Astronaut.average_age).to eq(34.0)
+    end
+  end
 end
